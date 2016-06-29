e30a903809c3b0822bab9a16c7b9b701597cb719
Michael Syson <masarapmabuhay@gmail.com>
2016-06-29 14:33:16 +0800

    +removed: English as an exception --> must first be purchased to unlock

--- src/usbong/android/utils/UsbongUtils.java
+++ src/usbong/android/utils/UsbongUtils.java
@@ -463,10 +463,11 @@ public class UsbongUtils {
 	//updated by Mike, 20160504
 	//must comply with JDK 1.6
 	public static boolean isLanguageIsAnException(String s) {
-		if (s.equals("English")) {
+/*		//commented out by Mike, 20160612
+ 		if (s.equals("English")) {
 			return true;
 		}
-		else if (s.equals("Filipino")) {
+		else */if (s.equals("Filipino")) {
 			return true;
 		}				
 		return false;
